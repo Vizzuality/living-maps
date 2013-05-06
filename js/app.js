b03980dbd499c65879d39a0dc0c988da0100e0f6
saleiva <saleiva@gmail.com>
2013-05-06 18:52:16 +0200

    x?

--- js/app.js
+++ js/app.js
@@ -5,6 +5,25 @@ var App = {
   old_time: 0,
   time: 0,
 
+  spin_opts: {
+    lines: 7, // The number of lines to draw
+    length: 0, // The length of each line
+    width: 6, // The line thickness
+    radius: 9, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb
+    speed: 0.9, // Rounds per second
+    trail: 53, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  },
+
   init_time: 0,
   last_time: 1419,
 
@@ -50,6 +69,10 @@ var App = {
       setTimeout(function() {
         self.add_debug();
       }, 4000);
+
+    // var target = document.getElementById('foo');
+    var spinner = new Spinner(this.spin_opts).spin($('.spinner_container'));
+
   },
 
   _initTestData: function() {
