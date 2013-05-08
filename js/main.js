61a5a087d96235776419e1503321a6cf636339cd
Carlos Matallín <matallo@vizzuality.com>
2013-05-08 15:46:53 +0200

    mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ) {
+    document.location = "/mobile.html";
+  }
+
   App.initialize({
     time_scale: 15 * 60,
     scale: 2.0,
